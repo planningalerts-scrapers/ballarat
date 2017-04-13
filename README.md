85af6d6f5e40e424af6f53232acadb9ffc142d5e
Eric Tam <eric.tam@traceyanderic.com>
2017-04-13 23:53:45 +1000

    added couple `submit` to the code, fetching second page to get `date_received` added more console output, added Gemfile, updated README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# City of Ballarat Council Scraper
+
+* Server - .NET
+* Cookie tracking - Yes
+* Pagnation - No
+* Javascript - Yes
+* Clearly defined data within a row - Yes
+* Scrape detail page - Yes
+
+Enjoy
--- scraper.rb
+++ scraper.rb
@@ -1,57 +1,49 @@
 require 'scraperwiki'
 require 'mechanize'
-require 'date'
 
-base_url = "https://eservices.ballarat.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/"
-url = "#{base_url}enquirylists.aspx"
-
-agent = Mechanize.new do |a|
-  a.verify_mode = OpenSSL::SSL::VERIFY_NONE # Disable SSL verification :(
-end
-
-first_page = agent.get url
-p first_page.title.strip
-first_page_form = first_page.forms.first
-first_page_form.radiobuttons.first.click
-summary_page = first_page_form.click_button
-
-p summary_page.title.strip
-
-
-das_data = []
-while summary_page
-  das_data = das_data + summary_page.root.css('.ContentPanel, .AlternateContentPanel').collect do |tr| 
-    tr.css('td').collect { |td| td.inner_text.strip }
-  end
-
-  next_page_img = summary_page.root.at_xpath("//a/img[contains(@src, 'nextPage')]")
-  summary_page = nil
-  if next_page_img
-    p "Found another page"
-    summary_page = agent.get "#{base_url}#{next_page_img.parent['href']}"
-  end
-end
-
+base_url    = "https://eservices.ballarat.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
+detail_url  = "https://eservices.ballarat.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/"
 comment_url = 'mailto:ballcity@ballarat.vic.gov.au'
 
-das = das_data.collect do |da_item|
-  page_info = {}
-  page_info['council_reference'] = da_item[0]
-  # There is a direct link but you need a session to access it :(
-  page_info['info_url'] = url
-  page_info['description'] = da_item[3]
-  page_info['address'] = "#{da_item[1]}, #{da_item[2]}"
-  page_info['date_scraped'] = Date.today.to_s
-  page_info['comment_url'] = comment_url
-  
-  page_info
-end
+agent = Mechanize.new
+agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-das.each do |record|
-  if ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? 
-    ScraperWiki.save_sqlite(['council_reference'], record)
-  else
-    puts "Skipping already saved record " + record['council_reference']
+# Select 'Planning Applications Currently on Advertising' and hit Next
+page = agent.get(base_url)
+form = page.form_with(id: "aspnetForm")
+form['mDataGrid:Column0:Property'] = 'ctl00$MainBodyContent$mDataList$ctl01$mDataGrid$ctl02$ctl00'
+form['ctl00$MainBodyContent$mContinueButton'] = 'Next'
+page = form.submit()
+
+# Hit the Search button
+form = page.form_with(id: "aspnetForm")
+form['ctl00$MainBodyContent$mGeneralEnquirySearchControl$mSearchButton'] = 'Search'
+page = form.submit()
+
+page.search("tr.ContentPanel, tr.AlternateContentPanel").each do |tr|
+  tr.search("a").each do |a|
+    # detail_page contain `date_received`
+    detail_page = agent.get(detail_url + a['href'].to_s)
+    date = detail_page.search('span.AlternateContentHeading:contains("Date Received")').first.parent.parent.search('div.AlternateContentText').inner_text.to_s.strip
+
+    record = {
+      'council_reference' => tr.search("a")[0].inner_text,
+      'address' => tr.search("td")[1].inner_text + ", " + tr.search("td")[2].inner_text + ", VIC",
+      'description' => tr.search("span.ContentText, span.AlternateContentText")[1].inner_text,
+      'info_url' => base_url,
+      'comment_url' => comment_url,
+      'date_scraped' => Date.today.to_s,
+      'date_received' => Date.parse(date).to_s,
+    }
+
+    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+      puts "Storing " + record['council_reference'] + " - " + record['address']
+#      puts record
+      ScraperWiki.save_sqlite(['council_reference'], record)
+    else
+      puts "Skipping already saved record " + record['council_reference']
+    end
   end
 end
+
 
