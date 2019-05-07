7206ccf5f3afbe72a5528d4346477200c3430dc3
Matthew Landauer <mlandauer@gmail.com>
2019-05-07 15:57:06 +1000

    Always save record as planningalerts now does versioning

--- scraper.rb
+++ scraper.rb
@@ -36,14 +36,8 @@ page.search("tr.ContentPanel, tr.AlternateContentPanel").each do |tr|
       'date_received' => Date.parse(date).to_s,
     }
 
-    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-      puts "Storing " + record['council_reference'] + " - " + record['address']
+    puts "Storing " + record['council_reference'] + " - " + record['address']
 #      puts record
-      ScraperWiki.save_sqlite(['council_reference'], record)
-    else
-      puts "Skipping already saved record " + record['council_reference']
-    end
+    ScraperWiki.save_sqlite(['council_reference'], record)
   end
 end
-
-
