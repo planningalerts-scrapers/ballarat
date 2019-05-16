5411e3dd13f51d9176205bcb64f62b77ece7226e
Matthew Landauer <mlandauer@gmail.com>
2019-05-16 14:19:16 +1000

    Set index

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,8 @@
 require '../epathway_scraper'
 
 scraper = EpathwayScraper.new(
-  base_url: "https://eservices.ballarat.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
+  base_url: "https://eservices.ballarat.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP",
+  index: 0
 )
 
 scraper.scrape_and_save
