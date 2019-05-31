b6c6703890d3f71829b6f5659dcfedfddda3fe5a
Matthew Landauer <mlandauer@gmail.com>
2019-06-01 05:11:28 +1000

    Upgrade to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,6 @@
 require 'epathway_scraper'
 
-EpathwayScraper::Scraper.scrape_and_save(
+EpathwayScraper.scrape_and_save(
   "https://eservices.ballarat.vic.gov.au/ePathway/Production",
   list_type: :advertising
 )
